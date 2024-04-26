178ba601e740007d74f9b3357b8ef42a9d6179ae
Lyon Till <lyon.till@microsoft.com>
2024-04-26 10:50:11 +0100

    feat: refactor route implementation

--- src/lib.rs
+++ src/lib.rs
@@ -2,9 +2,8 @@
 mod bindings;
 mod routes;
 
-use bindings::wasi::http::types;
-
 use crate::bindings::{exports::wasi::http::incoming_handler::Guest, wasi::http::types::*};
+use bindings::wasi::http::types;
 
 impl PartialEq for types::Method {
     fn eq(&self, other: &Self) -> bool {
@@ -17,6 +16,13 @@ impl PartialEq for types::Method {
 
 fn new_response(response_out: ResponseOutparam, status_code: StatusCode, content: String) {
     let headers = Fields::new();
+    headers
+        .set(
+            &"Content-Type".to_string(),
+            &["application/json".as_bytes().to_vec()],
+        )
+        .unwrap();
+
     let response = OutgoingResponse::new(headers);
     let body = response.body().expect("Outgoing response");
 
--- src/routes/environment.rs
+++
@@ -1,5 +0,0 @@
-use crate::StatusCode;
-
-pub fn get_environment() -> (StatusCode, String) {
-    (200, "Environment".to_string())
-}
